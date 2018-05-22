724816831112fa97624bca52afdb6641d41d12c5
Alex Kim <askim1019@gmail.com>
2018-05-21 20:42:06 -0500

    Add spotify and twitter modules

--- liri.js
+++ liri.js
@@ -1,1 +1,18 @@
 require("dotenv").config();
+require("node-spotify-api");
+require("twitter");
+
+
+var keys = require('./keys.js');
+
+var spotify = new Spotify(keys.spotify);
+var twitter = new Twitter(keys.twitter);
+
+
+console.log(twitter);
+console.log(spotify);
+
+
+console.log(module.filename);
+console.log(module.id);
+console.log(module.exports);
